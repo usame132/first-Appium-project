cc31f17c2d1b4dbddc43f436b3e42b362911130c
Usame Muhammed <ussama14863@gmail.com>
2024-09-22 17:46:46 +0300

    create File object for Apk

--- src/test/java/appiumBasics/RunNativeAppOnAndroid.java
+++ src/test/java/appiumBasics/RunNativeAppOnAndroid.java
@@ -5,6 +5,7 @@ import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -14,12 +15,14 @@ public class RunNativeAppOnAndroid {
     @Test
     public void openApp() throws MalformedURLException {
 
+        File app= new File("ApiDemos-debug.apk");
+
         DesiredCapabilities caps= new DesiredCapabilities();
         //  caps.setCapability("deviceName","usame"); // Mobile Capability Type kullanmak daha sağlıklı olur, string hatalardan kaçınmak için.
         // caps.setCapability("app","C:\\Users\\Usame Muhammed\\IdeaProjects\\AppiumV\\ApiDemos-debug.apk");
         // caps.setCapability("automationName","UiAutomator2");
          caps.setCapability(MobileCapabilityType.DEVICE_NAME,"usame");
-         caps.setCapability(MobileCapabilityType.APP,"C:\\\\Users\\\\Usame Muhammed\\\\IdeaProjects\\\\AppiumV\\\\ApiDemos-debug.apk");
+         caps.setCapability(MobileCapabilityType.APP,app.getAbsolutePath());
          caps.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
 
         AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"),caps); // Appiumun eski versiyonlarında server sonunua wd/hub ekleniyordu "http://127.0.0.1:4723/wd/hub"
