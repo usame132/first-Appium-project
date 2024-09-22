1f7c48228fdf4290e6e5cafc80a13a0fd305c3d1
Usame Muhammed <ussama14863@gmail.com>
2024-09-22 22:44:28 +0300

    Find element by Accessibility Id

--- src/test/java/appiumBasics/RunAppUsingPackage.java
+++ src/test/java/appiumBasics/RunAppUsingPackage.java
@@ -1,5 +1,6 @@
 package appiumBasics;
 
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
@@ -23,9 +24,10 @@ public class RunAppUsingPackage {
         caps.setCapability(AndroidMobileCapabilityType.APP_PACKAGE,"com.google.android.deskclock");
 
         AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"),caps);
-        driver.findElement(By.id("com.google.android.deskclock:id/fab")).click(); // id ile clock e tıklama
-        Thread.sleep(3000);
-        driver.findElement(By.className("android.widget.EditText")).sendKeys("ankara");
+//        driver.findElement(By.id("com.google.android.deskclock:id/fab")).click(); // id ile clock e tıklama
+//        Thread.sleep(3000);
+//        driver.findElement(By.className("android.widget.EditText")).sendKeys("ankara");
 
+            driver.findElement(MobileBy.AccessibilityId("Timer")).click();;
     }
 }
